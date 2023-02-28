173cd1b98b6df5aa7039364da44994d179c505f6
iarseny <122807698+iarseny@users.noreply.github.com>
2023-02-28 16:32:52 +0000

    lesson

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,1 +1,14 @@
+def calculator(number1, number2, sign):
+    if sign == "+":
+        return number1 + number2
+    elif sign == "-":
+        return number1 - number2
+    elif sign == "*":
+        return number1 * number2
+    elif sign == "/":
+        return number1 / number2
 
+a = input()
+b = input()
+s = input()
+print(calculator(int(a),int(b),s))
---
+++ lesson1/task1.py
@@ -0,0 +1,9 @@
+n = 0
+
+while n < 5:
+    print(n)
+    n += 1;
+
+
+for i in range(10, 0, -2):
+    print(i)
