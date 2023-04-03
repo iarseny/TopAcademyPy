45554ac27e8fa53f2af2b018328a6dac85cecd0b
iarseny <122807698+iarseny@users.noreply.github.com>
2023-04-03 20:17:45 +0300

    Update task1.py

--- homework 14/task1.py
+++ homework 14/task1.py
@@ -1,48 +1,58 @@
 size = int(input())
-
-s = [int(input()) for i in range(size)]
+res = [int(input()) for i in range(size)]
 
 otr = 0
 chet = 0
-nechet= 0
-if len(s) >= 3:
-    krat_3 = s[3]
-else:
-    krat_3 = 0
+nechet = 0
 
-maximum = [s[0], 0]
-minimum = [s[0], 0]
-
-for j, i in enumerate(s):
-    if i < 0:
-        otr += 1
-
-    if i % 2 == 0:
-        chet += 1
-    else:
-        nechet += 1
-
-    if j % 3 == 0 and j != 3 and len(s) >= 3:
-        krat_3 *= i
-
-    if i > maximum[0]:
-        maximum[0] = i
-        maximum[1] = j
-
-    if i < minimum[0]:
-        minimum[0] = i
-        minimum[1] = j
-
-
+index_to_3 = res[0]
+minimum = [res[0], 0]
+maximum = [res[0], 0]
 sum = 0
-if minimum[1] < maximum[1]:
+between = 0
+pol = []
+
+
+for i, j in enumerate(res):
+    if j < 0:
+        otr += j
+
+    if j % 2 == 0:
+        chet += j
+    else:
+        nechet += j
+
+    if i % 3 == 0 and i != 0:
+        index_to_3 *= j
+
+    if j > maximum[0]:
+        maximum[1] = i
+
+    if j < minimum[0]:
+        minimum[1] = i
+
+    if j > 0:
+        pol.append(i)
+
+
+if maximum > minimum:
     for i in range(minimum[1] + 1, maximum[1]):
-        sum += s[i]
+        if i == minimum[1] + 1:
+            between = res[i]
+        else:
+            between *= res[i]
 
 else:
-    
     for i in range(maximum[1] + 1, minimum[1]):
-        sum += s[i]
+        if i == maximum[1] + 1:
+            between = res[i]
+        else:
+            between *= res[i]
 
 
-print(otr, chet, nechet, krat_3, minimum[0] * maximum[0], sum)
+
+for i in range(pol[0] + 1, pol[len(pol) - 1]):
+    sum += res[i]
+
+
+print("Сумма отрицательных:", otr, "Сумма чётных:", chet, "Сумма нечётных:", nechet ,"Произведение с индексами % 3:", index_to_3, "Произведение между минимальным и максимальным:", between, "Сумма:", sum)
