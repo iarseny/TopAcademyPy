5b6e0bb84f510ffa9a3b02571c8494e7b4fb60af
iarseny <122807698+iarseny@users.noreply.github.com>
2023-04-07 18:32:59 +0300

    Update main.py

--- homework 16/main.py
+++ homework 16/main.py
@@ -1,17 +1,31 @@
 import tkinter
+from tkinter import ttk
+from currency_converter import CurrencyConverter
+from tkinter.messagebox import showinfo
 
 root = tkinter.Tk()
 
 root.geometry("400x400")
 root.title("Конвертер валют")
 
-var = tkinter.StringVar()
+c = CurrencyConverter()
+
+def convert():
+    showinfo("Ответ", str(round(c.convert(int(spinbox.get()), "RUB", languages_var.get()))))
 
 spinbox = tkinter.Spinbox(from_=1, to=1000)
 spinbox.pack()
 
-button = tkinter.Button(text="Узнать")
+languages = ["EUR", "USD", "BGN"]
+
+languages_var = tkinter.StringVar(value=languages[0])
+combobox = ttk.Combobox(textvariable=languages_var, values=languages)
+combobox.pack()
+
+
+button = tkinter.Button(text="Узнать(из рублей)", command=convert)
 button.pack()
+
 
 root.resizable(False, False)
 
